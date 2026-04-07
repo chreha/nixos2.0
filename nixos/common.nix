f6036b07e2ef255c21b8bbbce6917f30fc05300a
charlie <charles.hreha@gmail.com>
2026-04-07 00:14:41 -0400

    fixes

--- nixos/common.nix
+++ nixos/common.nix
@@ -20,6 +20,22 @@
   ];
   time.timeZone = "America/New_York";
 
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+
   nixpkgs = {
     # You can add overlays here
     overlays = [
@@ -84,6 +100,23 @@
   boot.loader.efi.canTouchEfiVariables = true;
   # Explicitly disable grub to prevent the error
   boot.loader.grub.enable = false;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
   environment.systemPackages = with pkgs; [
     vim
