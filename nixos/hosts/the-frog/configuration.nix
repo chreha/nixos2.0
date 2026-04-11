c6539073e687bce8bffce7ba665c0e34dab07c21
charlie <charles.hreha@gmail.com>
2026-04-11 19:23:47 -0400

    fix: the-frog networking name

--- nixos/hosts/the-frog/configuration.nix
+++ nixos/hosts/the-frog/configuration.nix
@@ -10,7 +10,7 @@
     inputs.self.nixosModules.common
   ];
 
-  networking.hostName = "the-toad"; # unique to the primary desktop system
+  networking.hostName = "the-frog"; # unique to the primary desktop system
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.05";
