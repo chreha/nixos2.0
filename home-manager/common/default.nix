831460350b833adc2163ef05c8c565fd6ead9545
charlie <charles.hreha@gmail.com>
2026-04-14 10:34:31 -0400

    sync

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -6,46 +6,6 @@
       allowUnfree = true;
     };
   };
-  home = {
-    packages = with pkgs; [
-      dracula-theme
-      nixfmt-rfc-style
-    ];
-  };
-
-  programs = {
-    home-manager = {
-      enable = true;
-    };
-    firefox = {
-      enable = true;
-      profiles.charlie = {
-        search.default = "ddg";
-        settings = {
-          "browser.startup.homepage" = "https://nixos.org";
-        };
-      };
-    };
-    plasma = {
-      enable = true;
-    };
-    konsole = {
-      enable = true;
-      defaultProfile = "NixProfile";
-
-      customColorSchemes = {
-        Dracula = "${inputs.dracula-konsole}/Dracula.colorscheme";
-      };
-      profiles.NixProfile = {
-        name = "NixProfile";
-        font = {
-          name = "JetBrainsMono Nerd Font";
-          size = 16; # Set your desired terminal font size here
-        };
-        colorScheme = "Dracula";
-      };
-    };
-  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
--- home-manager/profiles/charlie/charlie-on-the-toad/default.nix
+++ home-manager/profiles/charlie/charlie-on-the-toad/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [
     (inputs.self + /home-manager/profiles/charlie)
@@ -6,4 +6,44 @@
     (inputs.self + /home-manager/so-you-want-to/be-a-communicator)
     (inputs.self + /home-manager/so-you-want-to/be-a-gamer)
   ];
+  home = {
+    packages = with pkgs; [
+      dracula-theme
+      nixfmt-rfc-style
+    ];
+  };
+
+  programs = {
+    home-manager = {
+      enable = true;
+    };
+    firefox = {
+      enable = true;
+      profiles.charlie = {
+        search.default = "ddg";
+        settings = {
+          "browser.startup.homepage" = "https://nixos.org";
+        };
+      };
+    };
+    plasma = {
+      enable = true;
+    };
+    konsole = {
+      enable = true;
+      defaultProfile = "NixProfile";
+
+      customColorSchemes = {
+        Dracula = "${inputs.dracula-konsole}/Dracula.colorscheme";
+      };
+      profiles.NixProfile = {
+        name = "NixProfile";
+        font = {
+          name = "JetBrainsMono Nerd Font";
+          size = 16; # Set your desired terminal font size here
+        };
+        colorScheme = "Dracula";
+      };
+    };
+  };
 }
