8a27246ff99c2bbddb4c78a714f30e19688f197e
charlie <charles.hreha@gmail.com>
2026-04-14 16:12:49 -0400

    fix: troubleshooting filebrowser

--- modules/nixos/filebrowser-server.nix
+++ modules/nixos/filebrowser-server.nix
@@ -6,7 +6,10 @@
     volumes = [
       "/mnt/the-pond:/srv" # Point FileBrowser to your RAID
       "/var/lib/filebrowser/filebrowser.db:/database.db" # Persistent database
-      "/var/lib/filebrowser/settings.json:/config/settings.json"
+    ];
+    cmd = [
+      "--database"
+      "/database/filebrowser.db"
     ];
     user = "1000:100"; # Run as 'charlie' (uid 1000) and group 'users' (gid 100)
   };
