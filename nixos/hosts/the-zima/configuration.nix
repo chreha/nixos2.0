06ab4a1244938c62cb88048795918af5843f70a6
charlie <charles.hreha@gmail.com>
2026-04-14 13:14:35 -0400

    sync

--- nixos/hosts/the-zima/configuration.nix
+++ nixos/hosts/the-zima/configuration.nix
@@ -1,6 +1,7 @@
 # configuration for primary desktop system
 {
   inputs,
+  pkgs,
   ...
 }:
 {
@@ -17,6 +18,18 @@
   networking.networkmanager.enable = true;
 
   networking.firewall.allowedTCPPorts = [ 22 ];
+
+  # Enable software RAID support
+  boot.swraid.enable = true;
+
+  # Ensure the kernel module is available during boot
+  boot.initrd.availableKernelModules = [
+    "md_mod"
+    "raid1"
+  ];
+
+  # This allows NixOS to recognize the RAID members you found earlier
+  environment.systemPackages = with pkgs; [ mdadm ];
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "25.11";
