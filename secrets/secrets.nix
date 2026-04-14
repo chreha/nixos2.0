3a4d1f531d6a2aece65810fba78ce49a542ba2ff
charlie <charles.hreha@gmail.com>
2026-04-14 16:54:07 -0400

    secrets update

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,18 @@
 let
-  charlie_the_toad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH6qAH9JiDDDWzt9HZJ1s0VqUy0j8mme3A0NwH7kfhFc";
+  charlie_the_toad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAZTtndZISLL/CWmMauXvGvRDUXrRiwpMkmPuK5WzpV/";
   charlie_the_frog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAdfqOg31FOaLYFtR00JalRK/j7HRcHAYkXR3t3tnKcF";
+  charlie_the_zima = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzceb1zrugZVJqtKd5u6rCVWB5KfJCa6+2DgiqvVUX5";
   host_the_toad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJfXOo/Au0+4KbqgAGntELPtiI6CPgirwB5qJo7RU4aZ";
   host_the_frog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUgFgT982NrCWiL9Rg8vd9Bes6GpqJpPuLnyHy2z4yM";
+  host_the_zima = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINuVpmdcgKA8xx2Suvy8B6z7JrUu6/aWFeNd0myvGMBn";
 
   all_hosts = [
     charlie_the_toad
     charlie_the_frog
+    charlie_the_zima
     host_the_toad
     host_the_frog
+    host_the_zima
   ];
 in
 {
