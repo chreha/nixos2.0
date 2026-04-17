451467f723538a11df09c0800d8875b7053e71cf
charlie <charles.hreha@gmail.com>
2026-04-17 09:32:29 -0400

    test

--- nixos/hosts/the-frog/configuration.nix
+++ nixos/hosts/the-frog/configuration.nix
@@ -20,7 +20,6 @@
 
   users.users.charlie.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWc0PwJEhUp9iP475J94WohR3c1/8fp/B7AXs99lduc brian@two"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJkH8jsaaU4V0/+KETE1oTzgilMFq9Ka494e+xdZYAP github-personal@the-toad"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILyWgDz2yH+tyu6Z7e+/kVtbY4iW8aNJte6y8wwlaUjR github-personal@the-frog"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2ssoOZXTvcF355EWkYK0I3NLfPUU0F+ciLPKb3So0V github-personal@the-zima"
   ];
