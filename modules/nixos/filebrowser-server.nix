24cdcf0104f60c9ee145fbe9b26375d3980fbe69
charlie <charles.hreha@gmail.com>
2026-04-20 10:07:46 -0400

    filebrowser fixes

--- modules/nixos/filebrowser-server.nix
+++ modules/nixos/filebrowser-server.nix
@@ -5,7 +5,7 @@
     ports = [ "8080:80" ]; # Access via http://zimaboard-ip:8080
     volumes = [
       "/mnt/the-pond:/srv" # Point FileBrowser to your RAID
-      "/var/lib/filebrowser/filebrowser.db:/database.db" # Persistent database
+      "/var/lib/filebrowser/filebrowser.db:/database/filebrowser.db"
     ];
     cmd = [
       "--database"
@@ -13,6 +13,10 @@
     ];
     user = "1000:100"; # Run as 'charlie' (uid 1000) and group 'users' (gid 100)
   };
+  systemd.tmpfiles.rules = [
+    "d /var/lib/filebrowser 0775 1000 100 -"
+    "f /var/lib/filebrowser/filebrowser.db 0664 1000 100 -"
+  ];
 
   services.samba = {
     enable = true;
