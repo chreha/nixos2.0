995fe994ce351cef02c4c1651586b1a4e7d64d8d
charlie <charles.hreha@gmail.com>
2026-07-27 11:55:17 -0400

    valheim fix

--- modules/nixos/valheim.nix
+++ modules/nixos/valheim.nix
@@ -6,19 +6,27 @@
     "d /etc/valheim/config 0755 root root -"
     "d /etc/valheim/data 0755 root root -"
   ];
+
   virtualisation.oci-containers.containers.valheim = {
     image = "lloesche/valheim-server";
+
     extraOptions = [
       "--cap-add=sys_nice"
       "--stop-timeout 120"
     ];
+
+    autoStart = true;
+
     volumes = [
       "/etc/valheim/config:/config"
       "/etc/valheim/data:/opt/valheim"
     ];
+
     ports = [
-      "2456-2457:2456-2457/udp"
+      "2456:2456/udp"
+      "2457:2457/udp"
     ];
+
     environment = {
       SERVER_NAME = "ValheimOnTheZima";
       WORLD_NAME = "FinalRealm";
